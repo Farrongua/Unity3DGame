ad37617aca6ed4ee64ff22bf9cba66fb54efafe0
Farrongua <70178844+Farrongua@users.noreply.github.com>
2021-01-27 14:24:34 +0200

    Add files via upload

---
+++ Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+    bool GameEnded = false;
+    public float restartDelay = 1f;
+
+    public void GameOver() {
+        if(GameEnded == false)
+        {
+            GameEnded = true;
+            Debug.Log("GAME OVER");
+            Invoke("Restart", restartDelay);
+        }
+        
+    }
+
+    void Restart() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
